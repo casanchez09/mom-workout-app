6dca87db985d005a191c8d9611e98995b2ad6422
casanchez09 <casanchez09@gmail.com>
2026-08-12 09:42:59 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './mom-workout-app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
